c9b7420a8106d098c640d4bd82dc38aa675db92d
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2026-04-02 21:25:34 +0000

    estamos listos para cargar

--- cargar.sh
+++ cargar.sh
@@ -2,8 +2,8 @@ clear
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
 ssh-add -l
-git push -u bitbucket secundaria
+git push bitbucket main
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/proyecto_Git
 ssh-add -l
-git push -u github secundaria
+git push github main
