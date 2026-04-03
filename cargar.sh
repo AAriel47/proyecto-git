bcaf2638fee55803bdd388f544b31e79a3cf3ec9
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2026-04-03 21:30:02 +0000

    traer cargar.sh desde main

---
+++ cargar.sh
@@ -0,0 +1,11 @@
+#./bin/sh
+clear
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_ed25519
+ssh-add -l
+git push bitbucket main
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/proyecto_Git
+ssh-add -l
+git push github main
+
