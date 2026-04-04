63b43ceb387f187d5e14cd73d03c7dec17b7793a
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2026-04-04 01:23:46 +0000

    vamos

--- cargar.sh
+++ cargar.sh
@@ -5,10 +5,10 @@ eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
 ssh-add -l
 git push bitbucket $rama
-git pull
+git pull bitbucket $rama
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/proyecto_Git
 ssh-add -l
 git push github $rama
-git pull
+git pull github $rama
 
