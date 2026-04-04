24b7ac15205e57e32f8d028aea9ddceef9ff7a0d
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2026-04-04 01:35:37 +0000

    rama 4 carga.sh

--- cargar.sh
+++ cargar.sh
@@ -1,13 +1,14 @@
 #./bin/sh
+export rama=$(git rev-parse --abbrev-ref --short HEAD)
 clear
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
 ssh-add -l
-git push bitbucket cuatro
-git pull
+git push bitbucket $rama
+git pull bitbucket $rama
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/proyecto_Git
 ssh-add -l
-git push github cuatro
-git pull
+git push github $rama
+git pull github $rama
 
