8b994b5a96d29113a39fb8277a4368203d7f3499
Alejandro Ariel Uñate <alejandroarielunate@gmail.com>
2026-04-04 10:00:05 +0000

    listo cargar.sh

--- cargar.sh
+++ cargar.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 clear
 export rama=$(git symbolic-ref --short HEAD)
+if [ -z "$rama" ]; then
+	echo "Error: No estás en una rama válida (posible HEAD desasociado)."
+	exit 1
+fi
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
 ssh-add ~/.ssh/proyecto_Git
 ssh-add -l
-git pull --rebase bitbucket $rama
-git push bitbucket $rama
-git pull --rebase github $rama
-git push github $rama
+git pull --rebase bitbucket $rama && git push bitbucket $rama
+git pull --rebase github $rama && git push github $rama
 ssh-agent -k
